07fb34a14b6f5ca6ebb9368f33cf252d6bbad058
franco e <francoode@gmail.com>
2025-10-22 19:21:04 -0300

    feat/add docker and db

--- src/reservations/entities/sector.entity.ts
+++ src/reservations/entities/sector.entity.ts
@@ -1,11 +1,6 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany, CreateDateColumn, UpdateDateColumn } from 'typeorm';
-import { Restaurant } from './restaurant.entity';
-import { Table } from './table.entity';
-import { Reservation } from './reservation.entity';
 import { ISODateTime } from 'src/shared/types';
 
 
-@Entity('sectors')
 export class Sector {
   id: string;
   restaurantId: string;
